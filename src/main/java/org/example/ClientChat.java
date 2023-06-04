cc7e85da3ad4d47913bc3048d9c0ec749555ebb8
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-06-04 13:20:59 +0200

    anyfilev7

--- src/main/java/org/example/ClientChat.java
+++ src/main/java/org/example/ClientChat.java
@@ -267,6 +267,122 @@ package org.example;
 //        }
 //    }
 //}
+//import java.io.*;
+//import java.net.Socket;
+//
+//public class ClientChat {
+//    private static final String SERVER_HOST = "157.158.137.6";
+//    private static final int SERVER_PORT = 8080;
+//
+//    public static void main(String[] args) {
+//        try {
+//            Socket serverSocket = new Socket(SERVER_HOST, SERVER_PORT);
+//            System.out.println("Connected to chat server at " + SERVER_HOST + ":" + SERVER_PORT);
+//
+//            BufferedReader serverIn = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+//            PrintWriter serverOut = new PrintWriter(serverSocket.getOutputStream(), true);
+//
+//            BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
+//
+//            System.out.print("Enter your username: ");
+//            String username = consoleIn.readLine();
+//            serverOut.println(username);
+//
+//            Thread serverThread = new Thread(() -> {
+//                try {
+//                    String serverMessage;
+//                    while ((serverMessage = serverIn.readLine()) != null) {
+//                        System.out.println(serverMessage);
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//            serverThread.start();
+//
+//            String clientMessage;
+//            while ((clientMessage = consoleIn.readLine()) != null) {
+//                if (clientMessage.startsWith("/upload")) {
+//                    String filePath = clientMessage.substring("/upload ".length());
+//                    uploadFile(filePath, serverSocket.getOutputStream());
+//                } else if (clientMessage.startsWith("/download")) {
+//                    String fileName = clientMessage.substring("/download ".length());
+//                    serverOut.println(clientMessage);
+//                    downloadFile(fileName, username, serverIn);
+//                } else if (clientMessage.equals("/logout")) {
+//                    serverOut.println("/logout");
+//                    break;
+//                } else {
+//                    serverOut.println("[" + username + "]: " + clientMessage);
+//                }
+//            }
+//
+//            serverThread.join();
+//            serverSocket.close();
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private static void uploadFile(String filePath, OutputStream serverOut) {
+//        try {
+//            File file = new File(filePath);
+//            if (!file.exists()) {
+//                System.out.println("File not found: " + filePath);
+//                return;
+//            }
+//
+//            PrintWriter printWriter = new PrintWriter(serverOut, true);
+//            printWriter.println("/upload " + file.getName());
+//
+//            FileInputStream fileInput = new FileInputStream(file);
+//            byte[] buffer = new byte[8192];
+//            int bytesRead;
+//            while ((bytesRead = fileInput.read(buffer)) != -1) {
+//                serverOut.write(buffer, 0, bytesRead);
+//            }
+//
+//            serverOut.flush();
+//            fileInput.close();
+//
+//            System.out.println("File uploaded: " + file.getName());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private static void downloadFile(String fileName, String username, BufferedReader serverIn) {
+//        try {
+//            String response = serverIn.readLine();
+//            if (response.equals("FILE_FOUND")) {
+//                String clientFolder = "ClientFiles/" + username;
+//                File clientDir = new File(clientFolder);
+//                if (!clientDir.exists()) {
+//                    clientDir.mkdirs();
+//                }
+//
+//                String filePath = clientFolder + "/" + fileName;
+//                FileWriter fileWriter = new FileWriter(filePath);
+//
+//                String line;
+//                while ((line = serverIn.readLine()) != null) {
+//                    if (line.equals("/endfile")) {
+//                        break;
+//                    }
+//                    fileWriter.write(line);
+//                    fileWriter.write(System.lineSeparator());
+//                }
+//
+//                fileWriter.close();
+//                System.out.println("File downloaded: " + fileName);
+//            } else if (response.equals("FILE_NOT_FOUND")) {
+//                System.out.println("File not found: " + fileName);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
 import java.io.*;
 import java.net.Socket;
 
@@ -304,7 +420,7 @@ public class ClientChat {
             while ((clientMessage = consoleIn.readLine()) != null) {
                 if (clientMessage.startsWith("/upload")) {
                     String filePath = clientMessage.substring("/upload ".length());
-                    uploadFile(filePath, serverSocket.getOutputStream());
+                    uploadFile(filePath, serverSocket.getOutputStream(), serverIn);
                 } else if (clientMessage.startsWith("/download")) {
                     String fileName = clientMessage.substring("/download ".length());
                     serverOut.println(clientMessage);
@@ -324,7 +440,7 @@ public class ClientChat {
         }
     }
 
-    private static void uploadFile(String filePath, OutputStream serverOut) {
+    private static void uploadFile(String filePath, OutputStream serverOut, BufferedReader serverIn) {
         try {
             File file = new File(filePath);
             if (!file.exists()) {
@@ -346,6 +462,10 @@ public class ClientChat {
             fileInput.close();
 
             System.out.println("File uploaded: " + file.getName());
+
+            // Read response from the server
+            String response = serverIn.readLine();
+            System.out.println(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -383,5 +503,6 @@ public class ClientChat {
         }
     }
 }
+
 
 
--- src/main/java/org/example/ServerChat.java
+++ src/main/java/org/example/ServerChat.java
@@ -153,6 +153,140 @@ package org.example;
 //        }
 //    }
 //}
+//import java.io.*;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ServerChat {
+//    private static final int SERVER_PORT = 8080;
+//    private List<ClientHandler> clients;
+//
+//    public static void main(String[] args) {
+//        ServerChat server = new ServerChat();
+//        server.start();
+//    }
+//
+//    public void start() {
+//        try {
+//            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+//            System.out.println("Chat server started on port " + SERVER_PORT);
+//
+//            clients = new ArrayList<>();
+//
+//            while (true) {
+//                Socket clientSocket = serverSocket.accept();
+//                System.out.println("New client connected: " + clientSocket);
+//
+//                ClientHandler clientHandler = new ClientHandler(clientSocket);
+//                clients.add(clientHandler);
+//
+//                Thread clientThread = new Thread(clientHandler);
+//                clientThread.start();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private class ClientHandler implements Runnable {
+//        private Socket clientSocket;
+//        private BufferedReader clientIn;
+//        private PrintWriter clientOut;
+//        private String username;
+//
+//        public ClientHandler(Socket clientSocket) {
+//            try {
+//                this.clientSocket = clientSocket;
+//                clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                clientOut = new PrintWriter(clientSocket.getOutputStream(), true);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        public void run() {
+//            try {
+//                String clientMessage;
+//                while ((clientMessage = clientIn.readLine()) != null) {
+//                    if (clientMessage.startsWith("/upload")) {
+//                        String fileName = clientMessage.substring("/upload ".length());
+//                        receiveFile(fileName, clientSocket.getInputStream());
+//                    } else if (clientMessage.startsWith("/download")) {
+//                        String fileName = clientMessage.substring("/download ".length());
+//                        sendFile(fileName, clientOut);
+//                    } else if (clientMessage.equals("/logout")) {
+//                        System.out.println(username + " logged out from the chat.");
+//                        clients.remove(this);
+//                        break;
+//                    } else {
+//                        handleClientMessage(clientMessage);
+//                    }
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    if (!clientSocket.isClosed()) {
+//                        clientSocket.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//
+//        private void handleClientMessage(String clientMessage) {
+//            if (username == null) {
+//                username = clientMessage;  // Set the username when received from the client
+//                System.out.println(username + " entered the chat.");
+//            } else {
+//                String serverMessage = "[" + username + "]: " + clientMessage;
+//                System.out.println(serverMessage);
+//                // broadcast(serverMessage);
+//            }
+//        }
+//
+//        private void receiveFile(String fileName, InputStream clientInput) {
+//            try {
+//                File file = new File("ServerFiles/" + fileName);
+//
+//                FileOutputStream fileOutput = new FileOutputStream(file);
+//                BufferedOutputStream bufferedOutput = new BufferedOutputStream(fileOutput);
+//
+//                byte[] buffer = new byte[8192];
+//                int bytesRead;
+//                while ((bytesRead = clientInput.read(buffer)) != -1) {
+//                    bufferedOutput.write(buffer, 0, bytesRead);
+//                }
+//                System.out.println("File received: " + fileName);
+//                bufferedOutput.flush();
+//                bufferedOutput.close();
+//                fileOutput.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        private void sendFile(String fileName, PrintWriter clientOut) {
+//            try {
+//                Path filePath = Paths.get("ServerFiles/" + fileName);
+//                if (Files.exists(filePath)) {
+//                    clientOut.println("FILE_FOUND");
+//                    Files.lines(filePath).forEach(clientOut::println);
+//                    clientOut.println("/endfile");
+//                } else {
+//                    clientOut.println("FILE_NOT_FOUND");
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -215,7 +349,7 @@ public class ServerChat {
                 while ((clientMessage = clientIn.readLine()) != null) {
                     if (clientMessage.startsWith("/upload")) {
                         String fileName = clientMessage.substring("/upload ".length());
-                        receiveFile(fileName, clientSocket.getInputStream());
+                        receiveFile(fileName, clientSocket.getInputStream(), clientOut);
                     } else if (clientMessage.startsWith("/download")) {
                         String fileName = clientMessage.substring("/download ".length());
                         sendFile(fileName, clientOut);
@@ -251,7 +385,7 @@ public class ServerChat {
             }
         }
 
-        private void receiveFile(String fileName, InputStream clientInput) {
+        private void receiveFile(String fileName, InputStream clientInput, PrintWriter clientOut) {
             try {
                 File file = new File("ServerFiles/" + fileName);
 
@@ -269,11 +403,13 @@ public class ServerChat {
                 fileOutput.close();
 
                 System.out.println("File received: " + fileName);
+
+                // Send response to the client
+                clientOut.println("File received: " + fileName);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
         private void sendFile(String fileName, PrintWriter clientOut) {
             try {
                 Path filePath = Paths.get("ServerFiles/" + fileName);
